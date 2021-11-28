d048e4d9eaa107dffbb5d3a1e254e8a425a9e4ab
willemsleegers <w.sleegers@me.com>
2021-11-28 12:52:41 +0100

    Added HTML pages, added some validation checks to uploading a file, added download/print buttons to the results table.

--- app.R
+++ app.R
@@ -20,48 +20,69 @@ ui <- navbarPage(
     title = "statcheck // web", 
     collapsible = TRUE,
     header = tags$head(
-      tags$link(rel = "stylesheet", type = "text/css", href = "css/styles.css")
+      tags$link(rel = "stylesheet", type = "text/css", href = "css/styles.css"),
+      tags$script(src = "https://kit.fontawesome.com/0c3170759b.js", 
+        crossorigin = "anonymous")
     ),
     tabPanel("Home",
       fluidRow(
-        column(12,
-          includeHTML("html/home.html"),
-          hr(),
-          fileInput("file", 
-            label = "Upload files (pdf, html, or docx):",
-            multiple = FALSE,
-            accept = c('pdf/html/docx')
-          ),
-          checkboxInput("one_tail",
-            label = "Try to identify and correct for one-tailed tests?",
-            value = FALSE
-          ),
-          br(),
-          shiny::uiOutput("table"),
-          br(),
-          conditionalPanel(
-            condition = 'output.results',
-            br(),
-            downloadButton('download_data'),
-            br(),
-            br(),
-          ),
-          br(),
-          br()
+        tags$div(class = "center",
+          column(10, 
+            includeHTML("html/home.html"),
+            hr(),
+            fileInput("file", 
+              label = "Upload files (pdf, html, or docx):",
+              multiple = FALSE,
+              accept = c('pdf/html/docx')
+            ),
+            h5("Settings:", class = "settings"),
+            checkboxInput("one_tail",
+              label = "Try to identify and correct for one-tailed tests?",
+              value = FALSE
+            ),
+            hr(),
+            shiny::uiOutput("table")
+          ) 
         )
       )
     ),
     tabPanel("Documentation", 
-      includeHTML("html/documentation.html")
+      fluidRow(
+        tags$div(class = "center",
+          column(10, 
+            includeHTML("html/documentation.html")
+          )
+        )
+      )
     ),
     tabPanel("About/FAQ",
-      includeHTML("html/FAQ.html")
+      fluidRow(
+        tags$div(class = "center",
+          column(10, 
+            includeHTML("html/FAQ.html")
+          )
+        )
+      )
     ),
-    tabPanel("Contact"),
+    tabPanel("Contact",
+      fluidRow(
+        tags$div(class = "center",
+          column(10, 
+            includeHTML("html/contact.html")
+          )
+        )
+      )
+    ),
     tabPanel("MS Word Add-in",
-      includeHTML("html/word-addin.html")
+      fluidRow(
+        tags$div(class = "center",
+          column(10, 
+            includeHTML("html/word-addin.html")
+          )
+        )
+      )
     )
-)
+  )
 
 # Server ------------------------------------------------------------------
 
@@ -70,9 +91,14 @@ server <- function(input, output) {
   # Create a reactive variable for creating the results table and download 
   # feature
   results <- reactive({
-    
     # Get the file extension of the file
     file_extension <- tools::file_ext(input$file$name)
+    
+    # Check whether the user supplied a pdf, html, or Word file.
+    validate(
+      need(file_extension %in% c("pdf", "html", "doc", "docx"), 
+        "Please select a PDF, HTML, or Word file.")
+    )
     
     # Extract text from the file, depending on the file extension
     if (file_extension == "pdf") {
@@ -80,56 +106,61 @@ server <- function(input, output) {
     } else if (file_extension == "html")  {
       html <- paste(readLines(input$file$datapath), collapse = "\n")
       text <- htm2txt::htm2txt(html)
-    } else if (file_extension == "docx") {
+    } else if (file_extension %in% c("doc", "docx")) {
       word <- readtext::readtext(input$file$datapath)
       text <- word$text
-    } else {
-      error("Nope.")
     }
     
     # Run statcheck
     res <- statcheck::statcheck(text, OneTailedTests = input$one_tail)
     
+    # Check whether any results were found
+    validate(
+      need(nrow(res) > 0, "No results found.")
+    )
+    
     return(res)
   })
 
   # Detailed:
-  output$results <- DT::renderDataTable({
-    req(input$file)
-
-    res <- results()
-    res$Error <- ifelse(res$Error == FALSE, "Consistent", ifelse(
+  output$results <- DT::renderDataTable(
+    extensions = "Buttons", 
+    options = list(
+      dom = 'Bfrtip',
+      buttons = 
+        list('copy', 'print', list(
+          extend = 'collection',
+          buttons = list(
+            list(extend = 'csv', filename = input$file$name),
+            list(extend = 'excel', filename = input$file$name),
+            list(extend = 'pdf', filename = input$file$name)
+          ),
+          text = 'Download'
+        )
+      )
+    ),
+    {
+      req(input$file)
+  
+      res <- results()
+      
+      res$Error <- ifelse(res$Error == FALSE, "Consistent", ifelse(
       res$DecisionError == TRUE, "Decision Inconsistency", "Inconsistency"))
     
-    res <- subset(res, select = c(Source, Raw, Computed, Error))
-    
-    # Set the source equal to the file name
-    res$Source <- input$file$name
-
-    # Reduce width of long source names
-    res$Source[nchar(res$Source) > 35] <- gsub("(?<=^.{30}).*", " (...)",  
-        res$Source[nchar(res$Source) > 35], perl = TRUE)
-    
-    # Format the computer p-value column
-    res$Computed <- sprintf("%.05f", res$Computed)
-
-    # Create human-friendly column names
-    names(res) <- c("Source", "Statistical reference", "Computed p-value",
-      "Consistency")
-
-    return(res)
-  })
-
-  # Download statcheck results
-  output$download_data <- downloadHandler(
-    filename = function() {"statcheck.csv"},
-    content = function(file) {
-      write.csv(results(), file, row.names = FALSE)
+      res <- subset(res, select = c(Raw, Computed, Error))
+      
+      # Format the computer p-value column
+      res$Computed <- sprintf("%.05f", res$Computed)
+  
+      # Create human-friendly column names
+      names(res) <- c("Statistical reference", "Computed p-value", "Consistency")
+  
+      return(res)
     }
   )
   
   output$table <- renderUI({
-    div(DT::dataTableOutput("results", width = "100%"))
+    div(DT::dataTableOutput("results"))
   })
 }
 
